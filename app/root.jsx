d3549ba248a63e19d085f0847a685c31976f9e91
Danilo M Silva <daanilomsilvaa@gmail.com>
2022-12-22 13:13:02 +0000

    error boundary - json file renamed to jso as test

--- app/root.jsx
+++ app/root.jsx
@@ -1,4 +1,5 @@
 import {
+  Link,
   Links,
   LiveReload,
   Meta,
@@ -32,6 +33,35 @@ export default function App() {
           <MainNavigation />
         </header>
         <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
+
+export function ErrorBoundary({ error }) {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+        <title>An error occurred!</title>
+      </head>
+      <body>
+        <header>
+          <MainNavigation />
+        </header>
+        <main className="flex flex-col items-center justify-center text-center gap-10 bg-gradient-to-r from-red-500 to-blue-500 h-screen px-20">
+          <h1 className="text-4xl font-semibold text-white/75">
+            An unexpected error occurred!
+          </h1>
+          <h6 className="text-xl text-white/75">{error.message}</h6>
+          <Link to="/" className="text-white/75 font-semibold">
+            Go back to Home!
+          </Link>
+        </main>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
